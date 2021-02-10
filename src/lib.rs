82cd21c1f1af9ee511cbd98fe3e9841e5c52b9d7
Filip Dulic <filip.dulic@bloxico.com>
2021-02-10 14:24:50 -0600

    replace qrcodegen with qrcode crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use chain_crypto::{Ed25519Extended, SecretKey, SecretKeyError};
 use image::{DynamicImage, ImageBuffer, Luma};
-use qrcodegen::{QrCode, QrCodeEcc};
+use qrcode::{
+    render::{svg, unicode},
+    EcLevel, QrCode,
+};
 use std::fs::File;
 use std::io::{self, prelude::*};
 use std::path::Path;
@@ -48,44 +51,29 @@ impl KeyQrCode {
         // Using binary would make the QR codes more compact and probably less
         // prone to scanning errors.
         let enc_hex = hex::encode(enc);
-        let inner = QrCode::encode_text(&enc_hex, QrCodeEcc::High).unwrap();
+        let inner = QrCode::with_error_correction_level(&enc_hex, EcLevel::H).unwrap();
 
         KeyQrCode { inner }
     }
 
     pub fn write_svg(&self, path: impl AsRef<Path>) -> Result<(), KeyQrCodeError> {
         let mut out = File::create(path)?;
-        let svg = self.inner.to_svg_string(QR_CODE_BORDER);
-        out.write_all(svg.as_bytes())?;
+        let svg_file = self
+            .inner
+            .render()
+            .quiet_zone(true)
+            .dark_color(svg::Color("#000000"))
+            .light_color(svg::Color("#ffffff"))
+            .build();
+        out.write_all(svg_file.as_bytes())?;
         out.flush()?;
         Ok(())
     }
 
     pub fn to_img(&self) -> ImageBuffer<Luma<u8>, Vec<u8>> {
         let qr = &self.inner;
-
-        let inner_size_in_pixels = qr.size() * MODULE_SIZE;
-        let size = QR_CODE_BORDER * 2 + inner_size_in_pixels;
-
-        let mut img = ImageBuffer::from_pixel(size as u32, size as u32, Luma([255u8]));
-
-        for x in 0..qr.size() {
-            for y in 0..qr.size() {
-                if qr.get_module(x, y) {
-                    // draw a block square of module_size * module_size
-                    for i in 0..MODULE_SIZE {
-                        for j in 0..MODULE_SIZE {
-                            img.put_pixel(
-                                (x * MODULE_SIZE + i + QR_CODE_BORDER) as u32,
-                                (y * MODULE_SIZE + j + QR_CODE_BORDER) as u32,
-                                Luma([0u8]),
-                            );
-                        }
-                    }
-                }
-            }
-        }
-
+        let img = qr.render::<Luma<u8>>()
+            .build();
         img
     }
 
